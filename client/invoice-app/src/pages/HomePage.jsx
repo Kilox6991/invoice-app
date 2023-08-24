fa5f0da00da0d361e72c038d53e653dad09f4ae0
Kilox6991 <4inf.mpromero@gmail.com>
2023-08-24 19:41:38 +0200

    Homepage composition

--- client/invoice-app/src/pages/HomePage.jsx
+++ client/invoice-app/src/pages/HomePage.jsx
@@ -1,9 +1,26 @@
+import { Box, Container } from "@mui/material";
+
+import CssBaseline from '@mui/material/CssBaseline';
+
 function HomePage() {
-    return(
-        <div>HomePage</div>
+    return (
+        <>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1, minHeight: "100vh", background: "F8F8FB" }}>
+                <Container sx={{ display: "flex", justifyContent: "space-between", pt: "104px" }}>
+                    <div>
+                        <div>Invoices</div>
+                        <div style={{ fontSize: '14px' }}>7 invoices</div>
+                    </div>
+                    <div>Filter by status </div>
+                    <div>Boton New</div>
+                </Container>
+                <Container sx={{ pt: "32px",background:"FFFFF" }}>
+                    <div>Componente de factura</div>
+                </Container>
+            </Box>
+        </>
     )
 }
 
-export default HomePage
-
-
+export default HomePage;
